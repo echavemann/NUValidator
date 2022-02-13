3da97d972c482fa3d401926e4b650e230f8094cf
echavemann <echavemann@gmail.com>
2022-02-12 22:22:50 -0600

    Functions Complete!

--- Validation.py
+++ Validation.py
@@ -11,7 +11,30 @@ def domainvalidate(email):
     else:
         return 1
 
-print(domainvalidate("ashleylee@u.northwestern.edu"))
 
 def addressvalidate(email):
-    return 0
+    response = requests.get (
+        "https://isitarealemail.com/api/email/validate",
+        params = {'email' : email})
+    status = response.json()['status']
+    
+    if status == 'valid':
+        return 0
+    elif status == 'invalid':
+        return 1
+    else:
+        return 2
+
+
+
+def emailvalidate(email):
+    if "@" in email:
+        check1 = domainvalidate(email)
+        if check1 == 0:
+            check2 = addressvalidate(email)
+            if check2 == 0:
+                print ("Email verified!")
+                return 0
+            
+    print("Verification Failed.")
+    return 1
